6a7dbb1b1bcdc38b7dba0431ba09f16ce7d2cdcc
Rudy <peace1spirit@gmail.com>
2018-10-18 21:37:47 +0700

    firebase2

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Homepage from './views/Homepage.vue'
+ import Gamepage from './views/Gamepage.vue'
 
 Vue.use(Router)
 
@@ -20,6 +21,16 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/rooms',
+      name: 'rooms',
+      component: () => import('./views/Gamepage.vue'),
+      children: [{
+        path: ':id/:player',
+        name: 'room',
+        component: () => import('./views/Gamepage.vue')
+      }]
     }
   ]
 })

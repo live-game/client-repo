c2c3a1d176c6d0003c05abcbc0c7118068ab3e4d
Rudy <peace1spirit@gmail.com>
2018-10-18 21:46:00 +0700

    f

--- client/src/router.js
+++ client/src/router.js
@@ -4,7 +4,7 @@ import Homepage from './views/Homepage.vue'
 import Loadpage from './views/Loadpage.vue'
 
 Vue.use(Router)
-
+ 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,

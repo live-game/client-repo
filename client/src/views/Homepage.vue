76b34f5a5e80b5cc4debeb68c296a03b5707607c
Indira Dayo <indiradayo@live.com>
2018-10-19 14:41:13 +0700

    Tampilan ok tanpa bg sound dan gamepage

--- client/src/views/Homepage.vue
+++ client/src/views/Homepage.vue
@@ -1,40 +1,50 @@
 <template>
-    <!-- eslint disable -->
-    <div class="container">
-        <div class="row">
-            <!-- left section -->
-            <div class="col-lg-3 pinky">
-                <div class="list-group">
-                    <div class="form-group">
-                        <button class="form-control" data-toggle="modal" data-target="#exampleModal" data-backdrop="false">Add Room</button>
+
+    <div class="container-fluid">
+
+            <video autoplay muted loop id="myVideo">
+                <source type="video/mp4" src="../assets/videoplayback copy.mp4">
+                Your browser does not support HTML5 video.
+            </video>
+            <!-- <bgsound src="../assets/Futuristic background music -  within this digital maze.mp3" loop="infinite"></bgsound> -->
+            <!-- <audio controls autoplay loop>
+                <source src="../assets/Futuristic background music -  within this digital maze.mp3" type="audio/ogg">
+                <embed src="../assets/Futuristic background music -  within this digital maze.mp3" autostart="true" loop="true" hidden="true"> 
+            </audio> -->
+            <div class="row">
+            <div class="col-4  mx-auto d-block align-self-center" style="width: 18rem;">
+                <img class="my-img" src="../assets/New Project (1).png">
+            </div>
+            </div>
+            <div class="row">
+            <div class="card shadow mx-auto d-block align-self-center" style="width: 18rem;">
+                <div class="card-body">
+                    <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-backdrop="false">Add Room</a>
+                    <!-- <h5 class="card-title">Card title</h5> -->
+                    <p></p>
+                    <hr>
+                    <p>Rooms:</p>
+                    <div v-for="(room, index) in roomlist" :key="index">
+                        <a @click=joinRoom(room.roomid) class="btn btn-primary" data-toggle="modal" data-target="#joinModal" data-backdrop="false" >{{ 'Join' + ' ' + room.room }}</a>
                     </div>
                 </div>
-                <hr>            
-                    <div class="form-group" v-for="(room, index) in roomlist" :key="index">
-                        <button @click=joinRoom(room.roomid) class="form-control" data-toggle="modal" data-target="#joinModal" data-backdrop="false" >{{ 'Join' + ' ' + room.room }} </button>
-                    </div>                   
-                <hr>
-            </div>
-            <!-- left section end -->
 
-            <!-- supposed to be right section -->
-            <div class="col-lg-9">
-                <div class="row">
-                </div>
             </div>
-            <!-- right section end -->
-        </div>
+            </div>
 
-        <!-- modal add room-->
+        <!-- modal -->
+
+
         <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Type your room name below</h5>
+                        <h5 class="modal-title" id="exampleModalLabel">Type your name and room name below</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
+
                     <div class="modal-body">
                         <input type="text" v-model="roomName" class="form-control" placeholder="Room name">
                     </div>
@@ -48,10 +58,13 @@
                         <button type="button" class="btn btn-secondary" id="closebtn" data-dismiss="modal">Close</button>
                         <button @click="addRoom" type="button" class="btn btn-primary">Save changes</button>
                     </div>
+
+
                 </div>
             </div>
         </div>
         <!-- end modal -->
+
         <!-- modal join -->
         <div class="modal fade" id="joinModal" tabindex="-1" role="dialog" aria-hidden="true">
             <div class="modal-dialog" role="document">
@@ -66,8 +79,10 @@
                         <input type="text" v-model="nameplayer2" class="form-control" placeholder="Player name">
                     </div>
                     <div class="modal-footer">
+
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                         <button @click="joinProcess" type="button" class="btn btn-primary">Save changes</button>
+
                     </div>
                 </div>
             </div>
@@ -197,14 +212,87 @@ export default {
 </script>
 
 <style>
+* {
+    box-sizing: border-box;
+}
+
+.my-img {
+    width: 100%;
+}
+
+input {
+    margin-bottom: 5px;
+}
+.card{
+    background:rgba(4, 113, 255, 0.26) !important;
+    transition: transform .3s;
+
+    color: rgb(255, 255, 255);
+}
+.card:hover {
+    /* background:rgba(79, 176, 255, 0.356) !important; */
+    transform: scale(1.15)
+}
+.btn-primary {
+    margin-bottom: 10px;
+    background-color: rgb(15, 159, 255);
+    color: rgb(255, 255, 255) !important;
+}
+
+#myVideo {
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    min-width: 100%; 
+    min-height: 100%;
+    /* opacity: 0.85; */
+    
+}
+
+.content {
+    position: fixed;
+    bottom: 0;
+    background: rgba(0, 0, 0, 0.5);
+    color: #f1f1f1;
+    width: 100%;
+    padding: 20px;
+    
+}
+
+#myBtn {
+    width: 200px;
+    font-size: 18px;
+    padding: 10px;
+    border: none;
+    background: #000;
+    color: #fff;
+    cursor: pointer;
+}
+
+#myBtn:hover {
+    background: #ddd;
+    color: black;
+}
+
+
 /*!
 * Start Bootstrap - 3 Col Portfolio (https://startbootstrap.com/template-overviews/3-col-portfolio)
 * Copyright 2013-2018 Start Bootstrap
 * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-3-col-portfolio/blob/master/LICENSE)
 */
 
+
 body {
     padding-top: 54px;
+    margin: 0;
+    font-family: Arial;
+    font-size: 17px;
+    background: white;
+    /* background: rgb(19, 30, 54); */
+    /* background: linear-gradient(135deg, rgba(244,103,169,1) 0%, rgba(176,105,247,1) 100%); */
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
 }
 
 @media (min-width: 992px) {
@@ -221,12 +309,4 @@ body {
     margin-bottom: 30px;
 }
 
-.pinky {
-    background-color: pink !important;
-
-}
-
-.bluey {
-    background-color: rgb(0, 238, 255) !important;
-}
 </style>
